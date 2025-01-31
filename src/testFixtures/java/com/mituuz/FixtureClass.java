e1a4d7b8a98b1e9190d0f6debf7f04bbca34790d
mitja.leino <mitja.leino@hotmail.com>
2025-01-31 17:37:57 +0200

    Setup mockito

--- src/testFixtures/java/com/mituuz/FixtureClass.java
+++ src/testFixtures/java/com/mituuz/FixtureClass.java
@@ -1,7 +1,20 @@
 package com.mituuz;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class FixtureClass {
     public void helloFixture() {
         System.out.println("Hello from the fixture");
+
+        MockRecord mockRecord = mock(MockRecord.class);
+        when(mockRecord.getString()).thenReturn("There");
+        System.out.println(mockRecord.getString());
+    }
+
+    public record MockRecord() {
+        public String getString() {
+            return "Hello";
+        }
     }
 }
